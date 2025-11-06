7eaac8cf5cceb091e81cf40ab8ad4f4eee4ab0a2
Surya <suryajohnnadupuru@gmail.com>
2025-11-06 10:41:52 +0530

    Modified variable names

--- src/BasicFilteringAndMapping.java
+++ src/BasicFilteringAndMapping.java
@@ -5,7 +5,7 @@ public class BasicFilteringAndMapping {
 	
 	public static void main(String[] args) {
 		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-		numbers.stream().filter(a->a%2!=0).map(number->number*number).forEach(b->System.out.println(b));
+		numbers.stream().filter(num->num%2!=0).map(num->num*num).forEach(num->System.out.println(num));
 		
 	}
 
