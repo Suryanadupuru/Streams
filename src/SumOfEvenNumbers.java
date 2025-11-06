82ff051484d21e397c1538736dacc5bd7d2d9b50
Surya <suryajohnnadupuru@gmail.com>
2025-11-06 11:06:59 +0530

    Added Sum of even numbers class

---
+++ src/SumOfEvenNumbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+
+public class SumOfEvenNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		int sum=numbers.stream().filter(num->num%2==0).reduce(0, (num1,num2)->num1+num2);
+		System.out.println(sum);
+	}
+
+}
