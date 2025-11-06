f799074e8dddc7fde1e0a7476c3f61abc679bf04
Surya <suryajohnnadupuru@gmail.com>
2025-11-06 10:53:15 +0530

    String Manupulation class added

---
+++ src/StringManupulation.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class StringManupulation {
+	
+	public static void main(String[] args) {
+		List<String> words = Arrays.asList("apple", "banana", "avocado", "cherry", "apricot");
+		words.stream().map(String->String.toUpperCase()).filter(string->string.startsWith("A")).forEach(string->System.out.println(string));
+	}
+
+}
